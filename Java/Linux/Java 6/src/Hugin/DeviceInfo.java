3f84894c6a72fe3d0f8192900b5fa0e6b895823e
ayhandemirhan <ayhanformal@gmail.com>
2016-07-01 16:53:15 +0300

    Linux and Windows compatility
    RedHat 5 support

---
+++ Java/Linux/Java 6/src/Hugin/DeviceInfo.java
@@ -0,0 +1,13 @@
+package Hugin;
+
+public class DeviceInfo {
+  public String IP;
+  public int Port;
+  public String TerminalNo;
+  public String SerialNum;
+  public String Model;
+  public String Brand;
+  public String Version;
+};
+
+
