4e530ac5337f21132f0b00d75d6f4ba35cef8ef3
idris <idris.sehitoglu@hugin.com.tr>
2017-05-30 17:34:02 +0300

    GetEFTSlipCopy function exported to java

--- Java/Java 6/src/Hugin/FP300Service.java
+++ Java/Java 6/src/Hugin/FP300Service.java
@@ -142,6 +142,7 @@ public class FP300Service {
    public native String VoidEFTPayment(int acquierID, int batchNo, int stanNo);
    public native String RefundEFTPayment(int acquierID);
    public native String RefundEFTPayment(int acquierID, double amount);
+   public native String GetEFTSlipCopy(int acquierID, int batchNo, int stanNo, int zNo, int receiptNo);
    public native String GetBankListOnEFT();
    public native String GetSalesInfo();
 	public DateTime GetNewDateTime() { return new DateTime(); }
